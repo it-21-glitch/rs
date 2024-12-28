5ddd5a99ebccd04b307579bc0fda0b93dde3ec68
it-21@1bzimail.com <it-21@1bzimial.com>
2024-12-28 14:26:51 +0800

    更新

--- config/__init__.py
+++ config/__init__.py
@@ -1,6 +1,6 @@
 from .app import create_flask_app
 from .db_config import get_db
-from .settings import DevelopmentConfig, VerifyForm, VerifyUpdateForm, regular_function, rule_data_base, audit_status, \
+from .tools import DevelopmentConfig, VerifyForm, VerifyUpdateForm, regular_function, rule_data_base, audit_status, \
     generation_xlsx
 
 app = create_flask_app(DevelopmentConfig)
